3520cc2471f448ad36addab7c35f80444c525e3a
Natalia <baezanat@gmail.com>
2024-01-03 22:58:03 +0100

    lsp and autocompletions for nvim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,10 +1,12 @@
 return {
+  -- mason installs all our lsps
   {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup({})
     end
     },
+  -- mason-lspconfig bridges the gap b/n mason and nvim/lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
@@ -16,11 +18,17 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
       lspconfig.elixirls.setup({
-        cmd = { "/home/natalia/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" };
+        cmd = { "/home/natalia/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
+        capabilities = capabilities
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
